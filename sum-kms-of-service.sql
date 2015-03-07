6a36658769f3644daa066c5d5368ac8013fa7eb3
laidig <laidig@gmail.com>
2015-03-06 23:17:40 -0500

    script to calc scheduled kms of service

---
+++ sum-kms-of-service.sql
@@ -0,0 +1,5 @@
+select t.route_id, r.route_short_name, sum(l.shape_dist) from gtfs_shape_lengths l
+inner join gtfs_trips t on t.shape_id = l.shape_id
+inner join gtfs_routes r on r.route_id = t.route_id
+where t.service_id ilike '%weekday%'
+group by t.route_id, r.route_short_name limit 10
