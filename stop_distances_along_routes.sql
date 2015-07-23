1d8c9d7e0ccfb1564f4a119d4ca7b0470be97610
tony <laidig@gmail.com>
2015-07-22 22:52:43 -0400

    file name should say enough

--- stop_distances_along_routes.sql
+++ stop_distances_along_routes.sql
@@ -5,8 +5,8 @@ create table gtfs_dist_to_previous as
 select distinct
 t.route_id, d.stop_id, d.stop_sequence, round(dist_along_shape) as dist_along_shape
 , t.trip_headsign, t.shape_id, s.stop_name, 
- dist_along_shape - lag(dist_along_shape) 
-   over (order by t.route_id, t.shape_id, d.stop_sequence, d.stop_id) 
+ round(dist_along_shape - lag(dist_along_shape) 
+   over (order by t.route_id, t.shape_id, d.stop_sequence, d.stop_id)) 
    as dist_to_previous
 
  from gtfs_stop_distances_along_shape d 
@@ -17,4 +17,5 @@ order by t.route_id, t.shape_id, d.stop_sequence
 ;
 
 select * from gtfs_dist_to_previous 
-where (dist_to_previous > 0 or stop_sequence = 1); 
+where dist_to_previous > 0 
+or (stop_sequence = 1 and dist_to_previous = 0); 
